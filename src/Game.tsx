e1de2e70b45b5e57e76ddb4ac4918e816cc39ab8
gabriel-roque <gabrielrms.dll@gmail.com>
2022-06-19 14:55:03 -0300

    update game

--- src/Game.tsx
+++ src/Game.tsx
@@ -14,7 +14,7 @@ export const Game: React.FC = () => {
     canvasRef.width = 1000;
     canvasRef.height = 500;
 
-    k.debug.inspect = false;
+    k.debug.inspect = true;
 
     return k;
   }
@@ -37,6 +37,10 @@ export const Game: React.FC = () => {
         moviment: { from: 0, to: 1, loop: true, speed: 7 },
       },
     });
+
+    k.loadSprite('bg', './sprites/bg-mario.png');
+    k.loadSprite('grass-low', './sprites/grass-low.png');
+    k.loadSprite('grass-high', './sprites/grass-high.png');
   }
 
   useEffect(() => {
@@ -46,18 +50,29 @@ export const Game: React.FC = () => {
     let score = 0;
 
     k.scene('game', () => {
+      k.layers(['bg', 'grass', 'pipe', 'game'], 'game');
+
       const BASE_LINE = 55;
       const WIDTH_FLOOR = 204;
 
       // COMPONENTS;
       const mario = k.add([
         k.sprite('mario'),
+        k.layer('game'),
         k.pos(30, 352),
         k.scale(3),
         k.area(),
         k.body(),
       ]);
       mario.play('run');
+
+      k.add([
+        k.sprite('bg'),
+        k.layer('bg'),
+        k.scale(2),
+        k.pos(k.center().x, 250),
+        k.origin('center'),
+      ]);
 
       for (
         let index = 0;
@@ -77,19 +92,20 @@ export const Game: React.FC = () => {
         floor.play('moviment');
       }
 
-      const scoreLabel = k.add([
-        k.text(`Score: ${score}`, { size: 40 }),
-        k.pos(24, 24),
-      ]);
+      // const scoreLabel = k.add([
+      //   k.text(`Score: ${score}`, { size: 40 }),
+      //   k.pos(24, 24),
+      // ]);
 
-      k.onUpdate(() => {
-        score++;
-        scoreLabel.text = `Score: ${score}`;
-      });
+      // k.onUpdate(() => {
+      //   score++;
+      //   scoreLabel.text = `Score: ${score}`;
+      // });
 
       function spawnPipe() {
         k.add([
           k.sprite('pipe'),
+          k.layer('pipe'),
           k.scale(2),
           k.area(),
           k.pos(k.width(), k.height() - BASE_LINE),
@@ -101,9 +117,47 @@ export const Game: React.FC = () => {
       }
       spawnPipe();
 
+      function spawnGrass() {
+        function high() {
+          k.add([
+            k.sprite('grass-high'),
+            k.layer('grass'),
+            k.scale(2),
+            k.area(),
+            k.pos(k.width(), k.height() - BASE_LINE),
+            k.origin('botleft'),
+            k.move(k.LEFT, 100),
+            k.color(211, 169, 16),
+            'grass',
+          ]);
+          k.wait(k.rand(7, 10), () => high());
+        }
+
+        function low() {
+          k.add([
+            k.sprite('grass-low'),
+            k.layer('grass'),
+            k.scale(1.5),
+            k.area(),
+            k.pos(k.width(), k.height() - BASE_LINE),
+            k.origin('botleft'),
+            k.move(k.LEFT, 150),
+            'grass',
+          ]);
+          k.wait(k.rand(9, 12), () => low());
+        }
+
+        high();
+        low();
+      }
+      spawnGrass();
+
       setInterval(() => {
         k.get('pipe').forEach((pipe) => {
           if (pipe.pos.x <= -50) pipe.destroy();
+        });
+        k.get('grass').forEach((grass) => {
+          if (grass.pos.x <= -300) grass.destroy();
         });
       }, 1000);
 
@@ -116,7 +170,7 @@ export const Game: React.FC = () => {
       });
 
       mario.onCollide('pipe', () => {
-        k.go('lose');
+        // k.go('lose');
       });
 
       mario.onCollide('floor', () => {
